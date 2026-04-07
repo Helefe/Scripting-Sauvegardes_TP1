a22a5ae9e4dd900b48276e2d8f5f7d29cf29cb19
Loïc Faure <heleefe@outlook.fr>
2026-04-07 09:24:19 +0200

    commit

--- TP1/file.py
+++ TP1/file.py
@@ -1,4 +1,4 @@
-chemin = "C:\Users\helef\Desktop\Nouveau Document texte.txt"
+chemin = "C:\\Users\\helef\\Desktop\\Nouveau Document texte.txt"
 
 # Lire un fichier texte
 def lire_fichier(chemin):
@@ -10,3 +10,17 @@ def lire_fichier(chemin):
 def ecrire_fichier(chemin, contenu):
     with open(chemin, 'w', encoding='utf-8') as f:
         f.write(contenu)
+        
+def lire_fichier_binaire(chemin):
+    """Lit un fichier en mode binaire"""
+    with open(chemin, 'rb') as f:  # ??? mode?
+        contenu = f.read()
+    return contenu
+
+def obtenir_taille_fichier(chemin):
+    """Retourne la taille en octets"""
+    import os
+    return os.path.getsize(chemin)  # ??? utiliser os
+
+
+print(lire_fichier(chemin))
